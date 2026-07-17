e26853555ba30093072b8ec33d95e2d2b528f35b
pradeepdevops <pradeepdevopspractice@gmail.com>
2026-07-17 18:48:55 +0530

    added another word

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins Docker Kubernetes!"
+    return "Hello from Jenkins Docker Kubernetes and auto deploy!"
 
 @app.route("/health")
 def health():
